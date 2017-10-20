7eb1a497ec043e6ebee27a9f5343fe61b5cd827b
PAM-O\lenovo <pamela_ortiz@dlsu.edu.ph>
2017-10-21 01:48:41 +0800

    Edited GetResult

--- src/service/GetResult.java
+++ src/service/GetResult.java
@@ -1,16 +1,54 @@
 package service;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
+import bean.Book;
 
 public class GetResult {
 	
-	public static void getResult() {
+	public static List<Object[]> getResult() {
 		
+		/**
+		 * I'm trying to look for a solution
+		 * on how to make a query using multiple tables
+		 * i don't understand criteriabuilder thp
+		 */
 		
-	}
+		List<Object[]> queryList = null; 
+		
+					
+				EntityManagerFactory emf = Persistence.createEntityManagerFactory("mysqldb"); 
+				EntityManager em = emf.createEntityManager(); 
+				
+				CriteriaBuilder cb = emf.getCriteriaBuilder();
+				
+				EntityTransaction trans = em.getTransaction(); 
+				
+				try { 
+					trans.begin(); 
+				
+					CriteriaQuery<Book> q = cb.createQuery(Book.class); 
+					//queryList = q.getResultList(); 
+							
+					
+					trans.commit();
+				} catch (Exception e) { 
+					if( trans != null )
+						trans.rollback();
+					e.printStackTrace(); 
+				} finally { 
+					em.close();
+				}
+				
+				return queryList; 
+			}
 
 }
