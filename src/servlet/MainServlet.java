303e4b0878e01e630f3abb8d846d4463012b4495
jade-yu <jade_yu@dlsu.edu.ph>
2017-10-22 22:38:55 +0800

    added getting of query results

--- src/servlet/MainServlet.java
+++ src/servlet/MainServlet.java
@@ -1,14 +1,23 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.Q1;
+import service.GetResult;
+
 /**
  * Servlet implementation class MainServlet
  */
+
+@WebServlet(urlPatterns = {"/queries"})
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,7 +35,45 @@ public class MainServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		doPost(request, response);
+		String query = request.getParameter("query");
+		
+		System.out.println(query);
+        
+		switch(query) {
+		
+		default:
+		case "1":
+			request.setAttribute("result", GetResult.Query1());
+			break; 	
+		case "2": 
+			request.setAttribute("result", GetResult.Query2());
+			break;
+		case "3": 
+			request.setAttribute("result", GetResult.Query3("Seoul"));
+			break;
+		case "4": 
+			request.setAttribute("result", GetResult.Query4(2));
+			break;
+		case "5": 
+			request.setAttribute("result", GetResult.Query5());
+			break;
+		case "6": 
+			request.setAttribute("result", GetResult.Query6("Los Angeles"));
+			break;
+		case "7": 
+			request.setAttribute("result", GetResult.Query7());
+			break;
+		case "8": 
+			request.setAttribute("result", GetResult.Query8());
+			break;
+		
+		}
+
+		request.setAttribute("query", query);
+		request.setAttribute("time", GetResult.time);
+		
+        RequestDispatcher rd = request.getRequestDispatcher("Main.jsp");
+        rd.forward(request, response);
 	}
 
 	/**
@@ -34,7 +81,14 @@ public class MainServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+//		String urlPattern = request.getServletPath(); 
+//		
+//		System.out.println(urlPattern);
+//		
+//		switch(urlPattern) { 
+		//	case "/queries": getQueryOne(request, response); 
+			
+		}
 	}
 
-}
